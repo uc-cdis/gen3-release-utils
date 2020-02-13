14f96cf4eb79511b9515b7966a115f4a3b5755dd
Michael Ingram <mpingram92@gmail.com>
2020-02-13 00:16:23 -0600

    Diff portal config and manifest

--- manifest-diff.sh
+++ manifest-diff.sh
@@ -2,11 +2,12 @@
 
 if [ "$1" == "-h" ] || [ "$1" == "--help" ] || [ "$#" -ne 2 ]; then
   echo "------------------------------------------------------------------------------"
-  echo "Usage - gen3-config-diff <[repo/]namespace[:branch]> <[repo/]namespace[:branch]> [-(-m)anifest-only] [-(-p)ortal-only]"
+  echo "Usage - gen3-config-diff <[repo/]namespace[:branch]> <[repo/]namespace[:branch]>"
   echo "e.g., gen3-config-diff gen3.datastage.io:stable gitops-qa/qa-datastage.planx-pla.net"
   echo ""
   echo "This script diffs the configurations of two or more gen3 commons."
-  echo "NOTE: Requires jq https://stedolan.github.io/jq/"
+  echo ""
+  echo "NOTE: Requires jq https://stedolan.github.io/jq/ and vimdiff"
   echo ""
   echo "If 'repo' is not specified, it is assumed to be 'cdis-manifest'." 
   echo "If 'branch' is not specified, it is assumed to be 'master'"
@@ -17,6 +18,14 @@ fi;
 github_url_base="https://raw.githubusercontent.com/uc-cdis"
 default_branch="master"
 default_repo="cdis-manifest"
+diff_tool=vimdiff
+manifest_path="manifest.json"
+portal_config_path="portal/gitops.json"
+
+diff_manifest=true
+diff_portal=true
+
+declare -a urls
 
 # Iterate over arguments
 while test ${#} -gt 0
@@ -60,12 +69,46 @@ do
   namespace=$(echo $namespace | sed -r 's/^([^:]*):?.*$/\1/')
   echo "namespace:$namespace"
 
-  # Create the base URL
-  url="$github_url_base/$repo/$branch/$namespace"
-  echo $url
-
+  urls+=("$github_url_base/$repo/$branch/$namespace")
   shift
 done
+
+if [[ diff_manifest ]]
+  then
+    manifest_a_url="${urls[0]}/$manifest_path"
+    echo "Fetching manifest from $manifest_a_url..."
+    manifest_a_raw=$(curl -f $manifest_a_url)
+    # echo "$manifest_a_raw"
+    manifest_a_formatted=$(echo "$manifest_a_raw" | jq -S '.')
+
+    manifest_b_url="${urls[1]}/$manifest_path"
+    echo "Fetching manifest from $manifest_b_url..."
+    manifest_b_raw=$(curl -f $manifest_b_url)
+    # echo "$manifest_b_raw"
+    manifest_b_formatted=$(echo "$manifest_b_raw" | jq -S '.')
+    echo "$manifest_b_formatted"
+
+    vimdiff <(echo "$manifest_a_formatted") <(echo "$manifest_b_formatted")
+fi
+
+if [[ diff_portal ]]
+  then
+    portal_a_url="${urls[0]}/$portal_config_path"
+    echo "Fetching portal config from $portal_a_url..."
+    portal_a_raw=$(curl -f $portal_a_url)
+    # echo "$portal_a_raw"
+    portal_a_formatted=$(echo "$portal_a_raw" | jq -S '.')
+
+    portal_b_url="${urls[1]}/$portal_config_path"
+    echo "Fetching portal from $portal_b_url..."
+    portal_b_raw=$(curl -f $portal_b_url)
+    # echo "$portal_b_raw"
+    portal_b_formatted=$(echo "$portal_b_raw" | jq -S '.')
+    echo "$portal_b_formatted"
+
+    vimdiff <(echo "$portal_a_formatted") <(echo "$portal_b_formatted")
+fi
+
 
 # # Insert branch between repo and 'manifest.json', i.e. <repo>/master/<namespace>/manifest.json
 # manifest_A_branch=master
