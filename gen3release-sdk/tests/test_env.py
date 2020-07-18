c6c6b9011c395ba5cb63dfae92193ea7608ffc68
Marcelo Costa <marceloc@uchicago.edu>
2020-07-17 20:10:51 -0500

    wool is annoying sometimes

--- gen3release-sdk/tests/test_env.py
+++ gen3release-sdk/tests/test_env.py
@@ -2,10 +2,7 @@ from gen3release.config import env
 import pytest
 import json
 
-
 @pytest.fixture(scope="function")
 def env_obj():
     return env.Env("./data/test_environment.$$&")
 
-
-# def test___init__(): 
