87123121a395d60767e656a8e67542fc20cd7358
mattreex <mattreex.9@gail.com>
2020-07-27 13:10:20 -0500

    sort lists

--- gen3release-sdk/tests/test_env_cli.py
+++ gen3release-sdk/tests/test_env_cli.py
@@ -87,5 +87,5 @@ def test_copy_all_files():
         "manifests/fence/fence-config-public.yaml",
         "manifests/hatchery/hatchery.json",
     ]
-    assert files == expected_files
+    assert sorted(files) == sorted(expected_files)
     os.system(f"rm -r {fullpath_wd}/data/temp")
