571175a3aeda6dfaa53bbf0e8fbcf432ffeaa052
mattreex <mattreex.9@gail.com>
2020-08-17 11:46:49 -0500

    add default git token

--- gen3release-sdk/gen3release/gith/git.py
+++ gen3release-sdk/gen3release/gith/git.py
@@ -14,7 +14,7 @@ class Git:
         self,
         org="uc-cdis",
         repo=os.environ.get("REPO_NAME", "cdis-manifest"),
-        token=os.environ["GITHUB_TOKEN"].strip(),
+        token=os.environ("GITHUB_TOKEN", "MEH-123").strip(),
     ):
         """
      Creates a Github utils object to perform various operations against the uc-cdis repos and its branches, pull requests, etc.
