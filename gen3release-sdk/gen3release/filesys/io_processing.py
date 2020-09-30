9351099fa0a8b7d4750ea48144817466fcbf4fa9
mattreex <mattreex.9@gail.com>
2020-09-30 12:41:50 -0500

    weird missing edits

--- gen3release-sdk/gen3release/filesys/io_processing.py
+++ gen3release-sdk/gen3release/filesys/io_processing.py
@@ -124,13 +124,6 @@ def write_out_file(filepath, data, flag):
     logging.debug("Wrote file {}".format(filepath))
 
 
-def store_environment_params(data, env_obj, filename):
-
-    if filename == "manifest.json":
-        env_obj.load_sower_jobs(data)
-    return env_obj.load_environment_params(filename, data)
-
-
 def read_manifest(manifest):
     with open(manifest, "r") as m:
         contents = m.read()
@@ -194,21 +187,6 @@ def process_sower_jobs(mani_json, srcEnv_sowers, tgtEnv_sowers):
         if accountname:
             s_job["serviceAccountName"] = accountname
     return mani_json
-
-
-def clean_dictionary(dic):
-    """
-    Removes all keys in a nested dictionary that have null values
-    """
-    if not isinstance(dic, (dict, list)):
-        return dic
-    if isinstance(dic, list):
-        return [v for v in (clean_dictionary(v) for v in dic) if v or v == 0]
-    return {
-        k: v
-        for k, v in ((k, clean_dictionary(v)) for k, v in dic.items())
-        if v or v == 0
-    }
 
 
 def recursive_copy(srcEnv, tgtEnv, src, dst):
@@ -280,8 +258,6 @@ def recursive_copy(srcEnv, tgtEnv, src, dst):
                         src_data = merge(
                             tgtEnv.environment_specific_params[a_file], src_data
                         )
-                        # Remove any fields with no data
-                        src_data = clean_dictionary(src_data)
 
                         # Assure no keys without values
                         modified_file = True
