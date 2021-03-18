7bcd756acad0c65a1ab2d566c114df7b432fe1fc
Marcelo Costa <marceloc@uchicago.edu>
2021-03-18 12:16:06 -0500

    fix script

--- jenkins-jobs/generate_test_plan.sh
+++ jenkins-jobs/generate_test_plan.sh
@@ -4,6 +4,10 @@
 # Schedule: H 21 * * 2
 # Would last have run at Tuesday, March 16, 2021 9:47:46 PM UTC; would next run at Tuesday, March 23, 2021 9:47:46 PM UTC.
 
+git clone https://github.com/uc-cdis/gen3-qa.git
+
+cd gen3-qa
+
 export NAMESPACE="default"
 node --version
 npm install
