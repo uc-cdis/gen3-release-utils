1498ce99a64a7130bf85544a4e5308649a0a243f
Marcelo Costa <themarcelor@gmail.com>
2021-04-28 14:02:41 -0500

    remove variable notation escaping char (backslash)

--- jenkins-jobs/push-gen3-monthly-release-images-to-aws-ecr.sh
+++ jenkins-jobs/push-gen3-monthly-release-images-to-aws-ecr.sh
@@ -43,7 +43,7 @@ while IFS= read -r repo; do
 	    RC=$?
 	    if [ $RC != 0 ]; then
               echo "The Image is BROKEN\!"
-    	      curl -X POST --data-urlencode "payload={\\\"channel\\\": \\\"#gen3-qa-notifications\\\", \\\"username\\\": \\\"release-automation-watcher\\\", \\\"text\\\": \\\"THE IMAGE ${IMG_TO_PUSH} CANNOT BE PUSHED TO AWS ECR :red_circle: WHOEVER OWNS THIS IMAGE CAN YOU PLEASE INVESTIGATE?? \\\", \\\"icon_emoji\\\": \\\":facepalm:\\\"}" \$(g3kubectl get configmap global -o jsonpath={.data.ci_test_notifications_webhook})
+    	      curl -X POST --data-urlencode "payload={\\\"channel\\\": \\\"#gen3-qa-notifications\\\", \\\"username\\\": \\\"release-automation-watcher\\\", \\\"text\\\": \\\"THE IMAGE ${IMG_TO_PUSH} CANNOT BE PUSHED TO AWS ECR :red_circle: WHOEVER OWNS THIS IMAGE CAN YOU PLEASE INVESTIGATE?? \\\", \\\"icon_emoji\\\": \\\":facepalm:\\\"}" $(g3kubectl get configmap global -o jsonpath={.data.ci_test_notifications_webhook})
 	    fi
       done
 
@@ -60,7 +60,7 @@ while IFS= read -r repo; do
 	    RC=$?
 	    if [ $RC != 0 ]; then
               echo "The Image is BROKEN\!"
-    	      curl -X POST --data-urlencode "payload={\\\"channel\\\": \\\"#gen3-qa-notifications\\\", \\\"username\\\": \\\"release-automation-watcher\\\", \\\"text\\\": \\\"THE IMAGE ${IMG_TO_PUSH} CANNOT BE PUSHED TO AWS ECR :red_circle: WHOEVER OWNS THIS IMAGE CAN YOU PLEASE INVESTIGATE?? \\\", \\\"icon_emoji\\\": \\\":facepalm:\\\"}" \$(g3kubectl get configmap global -o jsonpath={.data.ci_test_notifications_webhook})
+    	      curl -X POST --data-urlencode "payload={\\\"channel\\\": \\\"#gen3-qa-notifications\\\", \\\"username\\\": \\\"release-automation-watcher\\\", \\\"text\\\": \\\"THE IMAGE ${IMG_TO_PUSH} CANNOT BE PUSHED TO AWS ECR :red_circle: WHOEVER OWNS THIS IMAGE CAN YOU PLEASE INVESTIGATE?? \\\", \\\"icon_emoji\\\": \\\":facepalm:\\\"}" $(g3kubectl get configmap global -o jsonpath={.data.ci_test_notifications_webhook})
 	    fi
       done
 
@@ -77,6 +77,6 @@ while IFS= read -r repo; do
   RC=$?
   if [ $RC != 0 ]; then
     echo "The Image is BROKEN\!"
-    curl -X POST --data-urlencode "payload={\\\"channel\\\": \\\"#gen3-qa-notifications\\\", \\\"username\\\": \\\"release-automation-watcher\\\", \\\"text\\\": \\\"THE IMAGE ${IMG_TO_PUSH} CANNOT BE PUSHED TO AWS ECR :red_circle: WHOEVER OWNS THIS IMAGE CAN YOU PLEASE INVESTIGATE?? \\\", \\\"icon_emoji\\\": \\\":facepalm:\\\"}" \$(g3kubectl get configmap global -o jsonpath={.data.ci_test_notifications_webhook})
+    curl -X POST --data-urlencode "payload={\\\"channel\\\": \\\"#gen3-qa-notifications\\\", \\\"username\\\": \\\"release-automation-watcher\\\", \\\"text\\\": \\\"THE IMAGE ${IMG_TO_PUSH} CANNOT BE PUSHED TO AWS ECR :red_circle: WHOEVER OWNS THIS IMAGE CAN YOU PLEASE INVESTIGATE?? \\\", \\\"icon_emoji\\\": \\\":facepalm:\\\"}" $(g3kubectl get configmap global -o jsonpath={.data.ci_test_notifications_webhook})
   fi
 done < "$repo_list"
