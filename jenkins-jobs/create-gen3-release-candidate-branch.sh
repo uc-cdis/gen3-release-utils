50a1b7fadda9e9b7c5d4e4d1229043e6593331f6
Jing Huang <71466688+jingh8@users.noreply.github.com>
2021-05-03 14:39:48 -0500

    add release version parameter to create-gen3-release-candidate-branch

--- jenkins-jobs/create-gen3-release-candidate-branch.sh
+++ jenkins-jobs/create-gen3-release-candidate-branch.sh
@@ -2,18 +2,18 @@
 
 # String parameter FORK_FROM
 #   Default value: master
+# String parameter RELEASE_VERSION
+#   Default value: 2021.06
 
 export GITHUB_USERNAME="themarcelor"
 # GITHUB_TOKEN obtained from Jenkins Credentials
 
-latest_release=$(git ls-remote --tags https://github.com/uc-cdis/fence | grep "/202" | grep -v "\^{}" | awk '{ print $2 }' | cut -d "/" -f3 | tail -n1)
 
 BRANCH_NAME=""
-if [[ $latest_release =~ [0-9]{4}\.([0-9]{2}) ]]; then
+if [[ $RELEASE_VERSION =~ [0-9]{4}\.([0-9]{2}) ]]; then
   echo "match"
   CONVERTED_MONHT_STR_TO_NUMBER=$(expr ${BASH_REMATCH[1]} + 0)
-  INCREMENTED_MONTH_NUM=$(( ( ($CONVERTED_MONTH_STR_TO_NUMBER) + 1 ) % 12 ));
-  BRANCH_NAME=$(printf "%02d\n" $INCREMENTED_MONTH_NUM)
+  BRANCH_NAME=$(printf "%02d\n" $CONVERTED_MONHT_STR_TO_NUMBER)
   echo "creating branch integration2021${BRANCH_NAME}..."
   ./make_branch.sh "$FORK_FROM" "integration2021${BRANCH_NAME}"
 else
