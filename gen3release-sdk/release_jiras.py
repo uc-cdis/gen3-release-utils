aad2e01528366f793d5bef5a7398e22ffbf120b7
Marcelo Costa <themarcelor@gmail.com>
2021-06-15 10:26:43 -0500

    fix syntax for selectlist

--- gen3release-sdk/release_jiras.py
+++ gen3release-sdk/release_jiras.py
@@ -99,7 +99,9 @@ COMPONENTS = [
 epic_dict = {
     "project": PROJECT_NAME,
     "customfield_10011": RELEASE_TITLE,
-    "customfield_10067": "'selectedValue=Project Team'",
+    "customfield_10067": [
+        {"name": "Product Team"},
+    ],
     "summary": RELEASE_TITLE,
     "description": "This epic comprises all the tasks releated to {}".format(
         RELEASE_TITLE
