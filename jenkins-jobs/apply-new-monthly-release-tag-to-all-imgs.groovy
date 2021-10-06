b775948a81cce6749834528f1173ee72133b0cb9
Marcelo Costa <themarcelor@gmail.com>
2021-10-06 16:33:59 -0500

    convert script to pipeline dsl with stages

--- jenkins-jobs/apply-new-monthly-release-tag-to-all-imgs.groovy
+++ jenkins-jobs/apply-new-monthly-release-tag-to-all-imgs.groovy
@@ -3,89 +3,109 @@
     e.g., 2021.04
 */
 
-println("### ## current path: ${env.WORKSPACE}");
-
-// Read the contents of repo_list.txt
-String fileContents = new File("${env.WORKSPACE}/repo_list.txt").getText('UTF-8')
-
-List<String> repos = Arrays.asList(LIST_OF_REPOS_WHOSE_IMAGES_NEED_TO_BE_TAGGED.split("\n"));
-
-def quietPeriod = 0;
-def jenkins = Jenkins.getInstance()
-def job = jenkins.getItem("quay-apply-new-tag-to-img");
-
-year = RELEASE_VERSION.split("\\.")[0]
-month = RELEASE_VERSION.split("\\.")[1]
-currentImg = "integration${year}${month}"
-
-reposAndImageNames = [
-    "pelican": "pelican-export",
-    "docker-nginx": "nginx",
-    "gen3-fuse": "gen3fuse-sidecar",
-    "cloud-automation": "awshelper",
-    "dataguids.org": "dataguids",
-]
-
-repos.each{ githubRepoName ->
-  imgName = githubRepoName
-  if (reposAndImageNames.containsKey(githubRepoName)) {
-    imgName = reposAndImageNames[githubRepoName];
-    println "Applying new image tag ${RELEASE_VERSION} to img ${imgName}...";
-
-    def params = []
-
-    params += new StringParameterValue("SERVICE_NAME", imgName);
-    params += new StringParameterValue("CURRENT_IMG_TAG", currentImg);
-    params += new StringParameterValue("NEW_IMG_TAG", RELEASE_VERSION);
-
-    def paramsAction = new ParametersAction(params);
-    hudson.model.Hudson.instance.queue.schedule(job, quietPeriod, null, paramsAction);
-    quietPeriod += 1;
-  }
-  else if (githubRepoName == "sower-jobs") {
-    sowerJobsImages=['metadata-manifest-ingestion', 'get-dbgap-metadata', 'manifest-indexing', 'download-indexd-manifest']
-
-    sowerJobsImages.each{ sowerJobsImg ->
-      println "Applying new image tag ${RELEASE_VERSION} to img from repo ${sowerJobsImg}...";
-
-      def params = []
-
-      params += new StringParameterValue("SERVICE_NAME", imgName);
-      params += new StringParameterValue("CURRENT_IMG_TAG", currentImg);
-      params += new StringParameterValue("NEW_IMG_TAG", RELEASE_VERSION);
-
-      def paramsAction = new ParametersAction(params);
-      hudson.model.Hudson.instance.queue.schedule(job, quietPeriod, null, paramsAction);
-      quietPeriod += 1;
+pipeline {
+    agent {
+      node {
+        label 'gen3-qa-worker'
+      }
     }
-  }
-  else if (githubRepoName == "mariner") {
-    marinerImages=['mariner-engine', 'mariner-s3sidecar', 'mariner-server']
+    stages {
+        stage('Clean old workspace') {
+            steps {
+                cleanWs()
+            }
+        }
+        stage('Iterate through all repos, find their img names and apply new tag') {
+            steps {
+                script {
+                    println("### ## current path: ${env.WORKSPACE}");
 
-    marinerImages.each{ marinerImg ->
-      println "Applying new image tag ${RELEASE_VERSION} to img from repo ${marinerImg}...";
+                    // Read the contents of repo_list.txt
+                    String fileContents = new File("${env.WORKSPACE}/repo_list.txt").getText('UTF-8')
 
-      def params = []
+                    List<String> repos = Arrays.asList(LIST_OF_REPOS_WHOSE_IMAGES_NEED_TO_BE_TAGGED.split("\n"));
 
-      params += new StringParameterValue("SERVICE_NAME", imgName);
-      params += new StringParameterValue("CURRENT_IMG_TAG", currentImg);
-      params += new StringParameterValue("NEW_IMG_TAG", RELEASE_VERSION);
+                    def quietPeriod = 0;
+                    def jenkins = Jenkins.getInstance()
+                    def job = jenkins.getItem("quay-apply-new-tag-to-img");
 
-      def paramsAction = new ParametersAction(params);
-      hudson.model.Hudson.instance.queue.schedule(job, quietPeriod, null, paramsAction);
-      quietPeriod += 1;
+                    year = RELEASE_VERSION.split("\\.")[0]
+                    month = RELEASE_VERSION.split("\\.")[1]
+                    currentImg = "integration${year}${month}"
+
+                    reposAndImageNames = [
+                        "pelican": "pelican-export",
+                        "docker-nginx": "nginx",
+                        "gen3-fuse": "gen3fuse-sidecar",
+                        "cloud-automation": "awshelper",
+                        "dataguids.org": "dataguids",
+                    ]
+
+                    repos.each{ githubRepoName ->
+                      imgName = githubRepoName
+                      if (reposAndImageNames.containsKey(githubRepoName)) {
+                        imgName = reposAndImageNames[githubRepoName];
+                        println "Applying new image tag ${RELEASE_VERSION} to img ${imgName}...";
+
+                        def params = []
+
+                        params += new StringParameterValue("SERVICE_NAME", imgName);
+                        params += new StringParameterValue("CURRENT_IMG_TAG", currentImg);
+                        params += new StringParameterValue("NEW_IMG_TAG", RELEASE_VERSION);
+
+                        def paramsAction = new ParametersAction(params);
+                        hudson.model.Hudson.instance.queue.schedule(job, quietPeriod, null, paramsAction);
+                        quietPeriod += 1;
+                      }
+                      else if (githubRepoName == "sower-jobs") {
+                        sowerJobsImages=['metadata-manifest-ingestion', 'get-dbgap-metadata', 'manifest-indexing', 'download-indexd-manifest']
+
+                        sowerJobsImages.each{ sowerJobsImg ->
+                          println "Applying new image tag ${RELEASE_VERSION} to img from repo ${sowerJobsImg}...";
+
+                          def params = []
+
+                          params += new StringParameterValue("SERVICE_NAME", imgName);
+                          params += new StringParameterValue("CURRENT_IMG_TAG", currentImg);
+                          params += new StringParameterValue("NEW_IMG_TAG", RELEASE_VERSION);
+
+                          def paramsAction = new ParametersAction(params);
+                          hudson.model.Hudson.instance.queue.schedule(job, quietPeriod, null, paramsAction);
+                          quietPeriod += 1;
+                        }
+                      }
+                      else if (githubRepoName == "mariner") {
+                        marinerImages=['mariner-engine', 'mariner-s3sidecar', 'mariner-server']
+
+                        marinerImages.each{ marinerImg ->
+                          println "Applying new image tag ${RELEASE_VERSION} to img from repo ${marinerImg}...";
+
+                          def params = []
+
+                          params += new StringParameterValue("SERVICE_NAME", imgName);
+                          params += new StringParameterValue("CURRENT_IMG_TAG", currentImg);
+                          params += new StringParameterValue("NEW_IMG_TAG", RELEASE_VERSION);
+
+                          def paramsAction = new ParametersAction(params);
+                          hudson.model.Hudson.instance.queue.schedule(job, quietPeriod, null, paramsAction);
+                          quietPeriod += 1;
+                        }
+                      } else {
+                        println "Applying new image tag ${RELEASE_VERSION} to img ${imgName}...";
+
+                        def params = []
+
+                        params += new StringParameterValue("SERVICE_NAME", imgName);
+                        params += new StringParameterValue("CURRENT_IMG_TAG", currentImg);
+                        params += new StringParameterValue("NEW_IMG_TAG", RELEASE_VERSION);
+
+                        def paramsAction = new ParametersAction(params);
+                        hudson.model.Hudson.instance.queue.schedule(job, quietPeriod, null, paramsAction);
+                        quietPeriod += 1;
+                      }
+                    }
+                }
+            }
+        }
     }
-  } else {
-    println "Applying new image tag ${RELEASE_VERSION} to img ${imgName}...";
-
-    def params = []
-
-    params += new StringParameterValue("SERVICE_NAME", imgName);
-    params += new StringParameterValue("CURRENT_IMG_TAG", currentImg);
-    params += new StringParameterValue("NEW_IMG_TAG", RELEASE_VERSION);
-
-    def paramsAction = new ParametersAction(params);
-    hudson.model.Hudson.instance.queue.schedule(job, quietPeriod, null, paramsAction);
-    quietPeriod += 1;
-  }
 }
