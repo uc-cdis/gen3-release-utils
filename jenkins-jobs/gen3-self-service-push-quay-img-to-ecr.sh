043e553c824dc48b2215780a854384a0d0217a0a
Atharva Rane <atharvar@uchicago.edu>
2022-01-03 15:27:29 -0500

    adding echo statements

--- jenkins-jobs/gen3-self-service-push-quay-img-to-ecr.sh
+++ jenkins-jobs/gen3-self-service-push-quay-img-to-ecr.sh
@@ -18,7 +18,9 @@ source $GEN3_HOME/gen3/gen3setup.sh
 
 repoExist=$(aws ecr describe-repositories | jq -r .repositories[].repositoryName | grep ${SERVICE_NAME})
 if [[ -n repoExist ]]; then
+  echo "create new ECR repo for ${SERVICE_NAME} ..."
 	gen3 ecr create-repository ${SERVICE_NAME}
+  echo "The Repo ${SERViCE_NAME} is created in AWS ECR"
 fi
 gen3 ecr quay-sync ${SERVICE_NAME} ${QUAY_VERSION}
 
