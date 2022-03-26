d72e79d86334189f34bb25351557a95057400fdd
haraprasadj <haraprasadj@uchicago.edu>
2022-03-25 21:14:30 -0700

    kill dd container

--- jenkins-jobs/gen3-run-load-tests.groovy
+++ jenkins-jobs/gen3-run-load-tests.groovy
@@ -263,6 +263,7 @@ pipeline {
                           node load-testing/loadTestRunner.js credentials.json load-testing/sample-descriptors/\$SELECTED_LOAD_TEST_DESCRIPTOR
 
                           echo "done"
+                          docker kill datadog
                         """
                         }
                     }
