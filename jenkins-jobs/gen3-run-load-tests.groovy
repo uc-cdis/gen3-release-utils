15b4de381828dd05c9cd6f89a434454ba4c8efd2
Atharva Rane <atharvar@uchicago.edu>
2022-03-31 15:29:55 -0400

    fixing

--- jenkins-jobs/gen3-run-load-tests.groovy
+++ jenkins-jobs/gen3-run-load-tests.groovy
@@ -73,17 +73,16 @@ pipeline {
                 ]){
                 sh """#!/bin/bash
 
-                  export KUBECTL_NAMESPACE="$TARGET_ENVIRONMENT"
                   # setup gen3 CLI
                   export GEN3_HOME=\$WORKSPACE/cloud-automation
                   source \$GEN3_HOME/gen3/gen3setup.sh
 
-                  indexdRecord=\$(curl "https://$KUBECTL_NAMESPACE/index/index" | jq -r '.records | length')
+                  indexdRecord=\$(curl "https://$TARGET_ENVIRONMENT/index/index" | jq -r '.records | length')
                   echo "----"
-                  echo $indexdRecord
+                  echo "$indexdRecord"
                   echo "----"
                   if [[ ${indexdRecord} -lt 0 ]];
-                    curl -X POST 'https://$KUBECTL_NAMESPACE/index/index' -H "$QA_DCP_CREDS_JSON" -H "Content-Type: application/json" -d \
+                    curl -X POST 'https://$TARGET_ENVIRONMENT/index/index' -H "$QA_DCP_CREDS_JSON" -H "Content-Type: application/json" -d \
                       '{"acl":[],"authz":["/abc/programs"],"file_name":"qa-test.txt","form":"object","hash":{"mds":"404e8919021a03285697647487f528ef"},"size":2681688756,"urls":["gs://dcf-integration-qa/qa-test.txt", "s3://cdis-presigned-url-test/testdata"]}' #pragma: allowlist secret
                   else
                     echo "There are more than 1 record in indexd. We should be good to go .."
