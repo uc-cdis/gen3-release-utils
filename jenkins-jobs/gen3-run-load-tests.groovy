ddd5a412d8720840f53cc0e0acf109aec246c7ed
Atharva Rane <atharvar@uchicago.edu>
2022-03-31 16:32:50 -0400

    fixing

--- jenkins-jobs/gen3-run-load-tests.groovy
+++ jenkins-jobs/gen3-run-load-tests.groovy
@@ -78,11 +78,13 @@ pipeline {
                   export GEN3_HOME=\$WORKSPACE/cloud-automation
                   source \$GEN3_HOME/gen3/gen3setup.sh
 
+                  export access_token=\$(gen3 api access-token cdis.autotest@gmail.com)
+
                   export indexdRecord=\$(curl "https://${TARGET_ENVIRONMENT}.planx-pla.net/index/index" | jq -r '.records | length')
                   echo \${indexdRecord}
 
                   if [[ \${indexdRecord} -le 0 ]]; then
-                    curl -X POST 'https://${TARGET_ENVIRONMENT}.planx-pla.net/index/index' -H "${QA_DCP_CREDS_JSON}" -H "Content-Type: application/json" -d \
+                    curl -X POST 'https://${TARGET_ENVIRONMENT}.planx-pla.net/index/index' -H "Authorization: Bearer ${access_token}" -H "Content-Type: application/json" -d \
                       '{"authz":["/programs/QA/projects/test"],"file_name":"qa-test.txt","form":"object","hashes":{"md5":"404e8919021a03285697647487f528ef"},"size":2681688756,"urls":["gs://dcf-integration-qa/qa-test.txt", "s3://cdis-presigned-url-test/testdata"]}' #pragma: allowlist secret
                   else
                     echo "There are more than 1 record in indexd. We should be good to go .."
