4bbc13c91b456c4e452d937a59a94ce1c48cf398
haraprasadj <haraprasadj@uchicago.edu>
2022-04-11 13:23:09 -0700

    try hostname

--- jenkins-jobs/dummy-gen3-run-load-tests.groovy
+++ jenkins-jobs/dummy-gen3-run-load-tests.groovy
@@ -103,7 +103,7 @@ pipeline {
                           export GEN3_SKIP_PROJ_SETUP=true
                           export RUNNING_LOCAL=false
                           export USE_DATADOG=true
-                          export K6_STATSD_ADDR=172.24.221.60:8125
+                          export K6_STATSD_ADDR=74a6674493ab:8125
 
                           DOCKER_CONTENT_TRUST=1 \
                           docker run -d \
