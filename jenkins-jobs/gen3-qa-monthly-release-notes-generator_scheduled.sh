8bc731015977e8a5df33f55e6e4d05ea7b21865b
Hara Prasad <haraprasadj@uchicago.edu>
2022-12-07 11:10:47 -0800

    Update gen3-qa-monthly-release-notes-generator_scheduled.sh

--- jenkins-jobs/gen3-qa-monthly-release-notes-generator_scheduled.sh
+++ jenkins-jobs/gen3-qa-monthly-release-notes-generator_scheduled.sh
@@ -11,9 +11,9 @@ pip3 install --editable git+https://github.com/uc-cdis/release-helper.git@gen3re
 
 export PATH=$PATH:/home/jenkins/.local/bin:/home/jenkins/.local/lib
 
-START_DATE=`date --date="41 day ago" +%Y-%m-%d`
-END_DATE=`date --date="14 day ago" +%Y-%m-%d`
-RELEASE_VERSION=`date --date="$END_DATE +1 month" +%Y.%m`
+START_DATE="Fri 14 Oct 2022 11:00:00 PM UTC"
+END_DATE="Fri 11 Nov 2022 11:00:00 PM UTC"
+RELEASE_VERSION="2022.12"
 RELEASE_NAME="Core Gen3 Release $RELEASE_VERSION"
 
 echo "### Generating Release Notes ###"
