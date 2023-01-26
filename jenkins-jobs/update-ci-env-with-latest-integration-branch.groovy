c7f3a6a8dc3d5615e2ba7afe47908a4301d4a2c7
Atharva Rane <41084525+atharvar28@users.noreply.github.com>
2023-01-26 12:52:08 -0500

    fixing the if loop

--- jenkins-jobs/update-ci-env-with-latest-integration-branch.groovy
+++ jenkins-jobs/update-ci-env-with-latest-integration-branch.groovy
@@ -90,12 +90,11 @@ spec:
                       export PATH=$PATH:/home/jenkins/.local/bin:/home/jenkins/.local/lib
                       pip3 install -U pip --user
                       poetry install
-                      if (TARGET_ENVIRONMENT.contains("niaid", "kidsfirst", "jcoin", "ibd")) {
+                      if [[ "$TARGET_ENVIRONMENT" == *"niaid"* ]] || [[ "$TARGET_ENVIRONMENT" == *"kidsfirst"* ]] || [[ "$TARGET_ENVIRONMENT" == *"jcoin"* ]] || [[ "$TARGET_ENVIRONMENT" == *"ibd"* ]]; then
                         poetry run gen3release apply -v $INTEGRATION_BRANCH -e ${WORKSPACE}/${REPO_NAME}/${TARGET_ENVIRONMENT} -pr "${PR_TITLE} ${TARGET_ENVIRONMENT} - ${INTEGRATION_BRANCH} $(date +%s)"
-                      }
-                      else { 
+                      else 
                        poetry run gen3release apply -v $INTEGRATION_BRANCH -e ${WORKSPACE}/${REPO_NAME}/${TARGET_ENVIRONMENT} -pr "${PR_TITLE} ${TARGET_ENVIRONMENT} - ${INTEGRATION_BRANCH} $(date +%s)" -l gen3-release
-                      }
+                      fi
                     '''
                 }
               }
