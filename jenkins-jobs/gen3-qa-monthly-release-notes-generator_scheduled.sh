8863bb49d8f5495b0cb1f58ea8d15941e0fb0dd8
Atharva Rane <41084525+atharvar28@users.noreply.github.com>
2023-08-10 15:48:20 -0400

    Update gen3-qa-monthly-release-notes-generator_scheduled.sh

--- jenkins-jobs/gen3-qa-monthly-release-notes-generator_scheduled.sh
+++ jenkins-jobs/gen3-qa-monthly-release-notes-generator_scheduled.sh
@@ -11,13 +11,13 @@ pip3 install --editable git+https://github.com/uc-cdis/release-helper.git@gen3re
 
 export PATH=$PATH:/home/jenkins/.local/bin:/home/jenkins/.local/lib
 
-# START_DATE="2023-05-13"
-# END_DATE="2023-07-14"
+START_DATE="2023-07-25"
+END_DATE="2023-07-31"
 RELEASE_VERSION="2023.09"
-# RELEASE_NAME="Core Gen3 Release $RELEASE_VERSION"
+RELEASE_NAME="Core Gen3 Release $RELEASE_VERSION"
 
-# echo "### Generating Release Notes ###"
-# bash ./jenkins-jobs/generate_release_notes.sh --startDate "$START_DATE" --endDate "$END_DATE" --releaseName "$RELEASE_NAME"
+echo "### Generating Release Notes ###"
+bash ./jenkins-jobs/generate_release_notes.sh --startDate "$START_DATE" --endDate "$END_DATE" --releaseName "$RELEASE_NAME"
 
 YEAR=$(echo $RELEASE_VERSION | cut -d"." -f 1)
 MONTH=$(echo $RELEASE_VERSION | cut -d"." -f 2)
@@ -44,5 +44,4 @@ python3 -m pip install dist/${wheel_file} --user
 
 cd $WORKSPACE
 
-# gen3_release_notes.md
-gen3release notes -v ${RELEASE_VERSION} -f manifest.json
+gen3release notes -v ${RELEASE_VERSION} -f gen3_release_notes.md manifest.json
